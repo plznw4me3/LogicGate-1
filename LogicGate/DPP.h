99b243790a274d6edfe257aa20d3c4c09d3f014a
김범준 <bjkim320@lycos.co.kr>
2016-05-31 22:15:09 +0900

    D플립플롭 추가

---
+++ LogicGate/DPP.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+class DPP {
+public:
+	CString ch[3] = { _T("D"), _T("Q"), _T("Q'") };
+	typedef struct DppLogic {
+		CPoint pos;		// Logic 위치
+		CPoint ch_pos[3];
+		int rotate;		// 회전
+		int input;		// 입력
+		int clk;		// 클럭
+		int output;		// 결과
+		CArray<POINT, POINT&> *in_line;
+		bool drawing;	// 그리는 중인가?
+		bool updown;	// 상승/하강 엣지
+	};
+	CArray<DppLogic, DppLogic&> logic;
+
+public:
+	void createLogic(CArray<DppLogic, DppLogic&> &logic, CPoint point);	// DPP Logic 만들기
+	bool makeOK(CArray<DppLogic, DppLogic&> &logic, CPoint point);	// 만들기 가능 유무
+	void rotateLogic(CArray<DppLogic, DppLogic&> &logic, CPoint poine);	// DPP Logic 돌리기
+	void drawLine(CArray<DppLogic, DppLogic&> &logic, CPoint point, bool &flag);	// 선 그리기
+	void drawLogic(DppLogic &logic, POINT *pt);	// Logic 그리기
+	void setValue(CArray<DppLogic, DppLogic&> &logic, int input, POINT point);	// input 입력
+	int getValue(DppLogic &logic);	// output 반환
+};
